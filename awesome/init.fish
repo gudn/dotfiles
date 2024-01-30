ac766a46e98cd5fabf0c4a7b2ccd0f3389c57aac
Daniil Novoselov <gudnmail@gmail.com>
2024-01-30 14:49:38 +0300

    awesome: fix touchpad id

--- awesome/init.fish
+++ awesome/init.fish
@@ -1,6 +1,6 @@
 xrandr --output eDP1 --mode 1920x1080 --rate 60 --preferred
 
-set -l touchpad_id 12
+set -l touchpad_id 20
 set -l natural_scrolling_id 314
 xinput set-prop $touchpad_id $natural_scrolling_id 1
 
