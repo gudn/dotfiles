b404d472b37f78e789f7b15ede147b663c563d7f
Daniil Novoselov <gudnmail@gmail.com>
2024-02-03 11:40:44 +0300

    awesome: detect touchpad id

--- awesome/init.fish
+++ awesome/init.fish
@@ -1,6 +1,6 @@
 xrandr --output eDP1 --mode 1920x1080 --rate 60 --preferred
 
-set -l touchpad_id 20
+set -l touchpad_id (xinput | awk 'match($0, /Touchpad\s+id=([0-9]+)/, ary) { print ary[1] }' | head -n 1)
 set -l natural_scrolling_id 314
 xinput set-prop $touchpad_id $natural_scrolling_id 1
 
