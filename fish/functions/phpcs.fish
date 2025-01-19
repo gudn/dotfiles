6014b368cb4cfe7ea9c55ab4426ebb4d55caa03e
Daniil Novoselov <me@gudn.link>
2025-01-19 04:13:38 +0300

    fish: remove unused functions

--- fish/functions/phpcs.fish
+++
@@ -1,4 +0,0 @@
-function phpcs --wraps='php ~/opt/phpcs.phar' --description 'alias phpcs php ~/opt/phpcs.phar'
-  php ~/opt/phpcs.phar $argv
-        
-end
--- fish/functions/retoken.fish
+++
@@ -1,3 +0,0 @@
-function retoken --wraps='ssh-add -e /usr/lib/opensc-pkcs11.so; ssh-add -s /usr/lib/opensc-pkcs11.so' --description 'alias retoken ssh-add -e /usr/lib/opensc-pkcs11.so; ssh-add -s /usr/lib/opensc-pkcs11.so'
-  ssh-add -e /usr/lib/opensc-pkcs11.so; ssh-add -s /usr/lib/opensc-pkcs11.so $argv; 
-end
